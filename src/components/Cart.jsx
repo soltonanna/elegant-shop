efd3b34a0a89caa24f2efab850cc50b74288f005
Anahit Sultanova <soltonanna@gmail.com>
2024-06-17 22:26:08 +0400

    add the full project

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,10 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import React, { useContext } from "react";
+import { CartContext } from "../store/shopping-cart-context";
+
+
+export default function Cart() {
+  const { items, updateItemQuantity } = useContext(CartContext);
+ 
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -20,11 +26,11 @@ export default function Cart({ items, onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,10 @@
-export default function Product({
-  id,
-  image,
-  title,
-  price,
-  description,
-  onAddToCart,
-}) {
+import React, { useContext } from "react";
+import { CartContext } from "../store/shopping-cart-context";
+
+export default function Product({ id, image, title, price, description }) {
+
+  const { addItemToCart } = useContext(CartContext);
+  
   return (
     <article className="product">
       <img src={image} alt={title} />
@@ -16,7 +15,7 @@ export default function Product({
           <p>{description}</p>
         </div>
         <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+          <button onClick={() => addItemToCart(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
